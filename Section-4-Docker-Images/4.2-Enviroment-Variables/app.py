cbe89a9291677afd7de689c39ac9299da4ec5c56
framirez90 <framirez.work@gmail.com>
2019-11-10 14:15:15 -0600

    section 4.2 fix

--- Section-4-Docker-Images/4.2-Enviroment-Variables/app.py
+++ Section-4-Docker-Images/4.2-Enviroment-Variables/app.py
@@ -1,13 +1,13 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 color = os.environ.get('APP_COLOR')
 
 @app.route("/")
 def main():
-	print(color)
-    return render.template('hello.html', color=color)
+    print(color)
+    return render_template('hello.html', color=color)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
